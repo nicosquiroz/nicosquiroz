2fbd4cb6eb6ed6c4f3c7c96e03db2d51e15d52d1
Nicolás S. Quiroz <nicosquiroz@gmail.com>
2024-12-10 14:27:56 -0300

    v1

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+import { BlogPosts } from 'app/components/posts'
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+        INNERTWIST
+      </h1>
+      <p className="mb-4">
+        {'Explicar qué es innertwist, quiénes somos y qué publicamos aquí. Trabajos terminados? O procesos de aprendizaje?'}
+        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
+        Vim's keystroke commands and tabs' flexibility for personal viewing
+        preferences. This extends to my support for static typing, where its
+        early error detection ensures cleaner code, and my preference for dark
+        mode, which eases long coding sessions by reducing eye strain.`}
+      </p>
+      <div className="my-8">
+        <BlogPosts />
+      </div>
+    </section>
+  )
+}
